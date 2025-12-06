d5f321fbcf8e889f74b3c6c1f5d3b10eb900f257
kocmidovam <32037668+kocmidovam@users.noreply.github.com>
2025-12-06 14:21:05 +0100

    update dependencies and fix Analytics Suspense

--- src/app/components/Email.tsx
+++ src/app/components/Email.tsx
@@ -1,50 +1,40 @@
-import React, { useState } from "react";
-import GithubIcon from "../../../public/github-icon.svg";
-import LinkedinIcon from "../../../public/linkedin-icon.svg";
-import Link from "next/link";
-import Image from "next/image";
+import GithubIcon from "../../../public/github-icon.svg"
+import LinkedinIcon from "../../../public/linkedin-icon.svg"
+import Link from "next/link"
+import Image from "next/image"
 
 const EmailSection = () => {
-
   return (
     <section
-      id="contact"
-      className="grid md:grid-cols-2 my-12 md:my-12 py-8 md:py-20 gap-12 md:gap-4 relative"
-    >
-      <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-full w-96 z-0 blur-3xl absolute top-1/4  -left-4 transform -translate-x-1/2 -translate-1/2"></div>
-      <div className="z-10">
-        <h5 className="text-xl font-bold text-white my-2">
-          Let&apos;s Connect
-        </h5>
-        <p className="text-[#ADB7BE] mb-4 max-w-md md:text-lg">
+      id='contact'
+      className='grid md:grid-cols-2 my-12 md:my-12 py-8 md:py-20 gap-12 md:gap-4 relative'>
+      <div className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-full w-96 z-0 blur-3xl absolute top-1/4  -left-4 transform -translate-x-1/2 -translate-1/2'></div>
+      <div className='z-10'>
+        <h5 className='text-xl font-bold text-white my-2'>Let&apos;s Connect</h5>
+        <p className='text-[#ADB7BE] mb-4 max-w-md md:text-lg'>
           {" "}
-          I&apos;m currently looking for new opportunities, my inbox is always
-          open. Whether you have a question or just want to say hi, I&apos;ll
-          try my best to get back to you!
+          I&apos;m currently looking for new opportunities, my inbox is always open. Whether you
+          have a question or just want to say hi, I&apos;ll try my best to get back to you!
         </p>
-        <div className="socials flex flex-row gap-2">
-          <Link href="https://github.com/kocmidovam" target="_blank">
-            <Image src={GithubIcon} alt="Github Icon" />
+        <div className='socials flex flex-row gap-2'>
+          <Link href='https://github.com/kocmidovam' target='_blank'>
+            <Image src={GithubIcon} alt='Github Icon' />
           </Link>
-          <Link
-            href="https://www.linkedin.com/in/marta-kocmidova/"
-            target="_blank"
-          >
-            <Image src={LinkedinIcon} alt="Linkedin Icon" />
+          <Link href='https://www.linkedin.com/in/marta-kocmidova/' target='_blank'>
+            <Image src={LinkedinIcon} alt='Linkedin Icon' />
           </Link>
         </div>
       </div>
-      <div className="flex justify-center items-center">
+      <div className='flex justify-center items-center'>
         <Link
-          href="mailto:kocmidova.marta@seznam.cz"
-          target="_blank"
-          className="transition-all z-10 text-white font-medium py-2.5 px-5 rounded-lg w-full  bg-gradient-to-r from-primary-900 to-secondary-400 hover:bg-gradient-to-tr w-max h-max"
-        >
+          href='mailto:kocmidova.marta@seznam.cz'
+          target='_blank'
+          className='transition-all z-10 text-white font-medium py-2.5 px-5 rounded-lg w-full  bg-gradient-to-r from-primary-900 to-secondary-400 hover:bg-gradient-to-tr w-max h-max'>
           Send Message
         </Link>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default EmailSection;
+export default EmailSection
